d7e85dce4c39c72374a6c9831c9eb55b5a330151
da4a <dankossss@gmail.com>
2020-01-17 16:32:17 +0200

    Math

---
+++ math/src/Math.java
@@ -0,0 +1,14 @@
+public class Math {
+
+    public static final double PI = 3.14;
+    public static final double G = 9.8;         // Прискорення вільного падіння (нормальне)
+
+    public static double circleArea(int radius) {
+        return PI * radius * radius;
+    }
+
+    public static double circleLength(int radius) {
+        return 2 * PI * radius;
+    }
+
+}
